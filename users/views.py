94821888682fd2daef1ea1e2eafbf45ad324d491
chaitanyahardikar <chaitanyahardikar@gmail.com>
2021-01-27 22:32:22 +0530

    Added profile page

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from blog.models import Post 
+from django.contrib.auth.models import User
 
 
 def register(request):
@@ -17,7 +19,7 @@ def register(request):
 	return render(request, 'users/register.html', {'form': form})
 
 @login_required
-def profile(request):
+def profile(request, pk):
 	if(request.method == 'POST'):
 		u_form = UserUpdateForm(request.POST, instance=request.user)
 		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
@@ -32,9 +34,15 @@ def profile(request):
 		u_form = UserUpdateForm(instance=request.user)
 		p_form = ProfileUpdateForm(instance=request.user.profile)
 
+	user = request.user
+	profileuser = User.objects.get(pk=pk)
+	user_posts = Post.objects.filter(author=profileuser).order_by('-date_posted')
 	context = {
 		'u_form': u_form,
-		'p_form': p_form
+		'p_form': p_form,
+		'user_posts' : user_posts,
+		'user' : user,
+		'profileuser' : profileuser
 	}
 
 	return render(request, 'users/profile.html', context)
